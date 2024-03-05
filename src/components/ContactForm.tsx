5996010e2839c79646a857d1dc8e1b0411828452
Bernd Bohmeier <berndbohmeier@googlemail.com>
2024-03-05 23:35:00 +0100

    Add state for sending/error

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,28 +1,43 @@
 import emailjs from "@emailjs/browser";
 import { useState } from "react";
+import type { FormEvent } from "react";
+
+enum State {
+  Input = "Input",
+  Sending = "Sending",
+  Sent = "Sent",
+  Error = "Error",
+}
 
 export default function ContactForm() {
-  const [sent, setSent] = useState(false);
+  const [state, setState] = useState(State.Input);
 
-  async function handleSubmit(e) {
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault();
+    if (state !== State.Input) return;
 
-    const form = e.target;
+    setState(State.Sending);
+
+    const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
 
     const formJson = Object.fromEntries(formData.entries());
-    await emailjs.send(
-      "default_service",
-      "template_aillhuc",
-      {
-        from_name: formData.name,
-        from_email: formData.email,
-        message: formData.message,
-      },
-      { publicKey: "YGL6jQ4ZZE8y_Ihth" },
-    );
-
-    setSent(true);
+    try {
+      await emailjs.send(
+        "default_service",
+        "template_aillhuc",
+        {
+          from_name: formJson.name,
+          from_email: formJson.email,
+          message: formJson.message,
+        },
+        { publicKey: "YGL6jQ4ZZE8y_Ihth" },
+      );
+      setState(State.Sent);
+    } catch (e) {
+      setState(State.Error);
+      console.log(e);
+    }
   }
 
   return (
@@ -30,11 +45,13 @@ export default function ContactForm() {
       <p className="text-l mb-10 text-center text-main">
         Interested in joining a training? Other questions? Let us know!
       </p>
-      {sent ? (
-        <>
-          <p className="text-l mb-10 text-center">Thanks for contacting us!</p>
-        </>
-      ) : (
+      {state === State.Sent && (
+        <p className="text-l mb-10 text-center">Thanks for contacting us!</p>
+      )}
+      {state === State.Error && (
+        <p className="text-l mb-10 text-center">Something went wrong.</p>
+      )}
+      {(state === State.Input || state === State.Sending) && (
         <form className="mx-auto max-w-md" onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="mb-2 block">
@@ -74,8 +91,9 @@ export default function ContactForm() {
           <button
             type="submit"
             className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+            disabled={state === State.Sending}
           >
-            Submit
+            {state === State.Sending ? "Sending..." : "Submit"}
           </button>
         </form>
       )}
