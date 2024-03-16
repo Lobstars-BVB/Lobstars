76d91cee1bc32d7f2f19ddd8514d72c7440e4528
karlkristopher <karl.secen@gmail.com>
2024-03-16 08:44:06 +0100

    add site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://lobstars-frisbee.club",
+  integrations: [tailwind(), react(), sitemap()],
 });
