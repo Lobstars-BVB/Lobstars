b2805e133c98989950ec6b717c41757034bdfd0e
Andrei Comaneci <aco.aco.ro@gmail.com>
2025-03-01 19:17:38 +0100

    Improve schema validation code
    * infer types to avoid casting
    * follow variable naming convention

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,37 +1,25 @@
 import { z } from 'zod';
 
-interface Reference {
-  sourceName: string;
-  link: string;
-  locator: string;
-}
-
-export interface QuizQuestion {
-  question: string;
-  answers: string[];
-  correctIndex: number;
-  explanation: string;
-  reference: Reference;
-}
-
-const ReferenceSchema: z.ZodType<Reference> = z.object({
+const referenceSchema = z.object({
   sourceName: z.string().trim().min(1, "The source should not be empty"),
   link: z.string().trim().min(1, "The link should not be empty").url("Invalid URL format"),
   locator: z.string().trim().min(1, "The locator should not be empty"),
 });
 
-const QuizQuestionSchema: z.ZodType<QuizQuestion> = z.object({
+const quizQuestionSchema = z.object({
   question: z.string().trim().min(10, "The question should be at least 10 characters long"),
   answers: z.array(z.string().trim().min(1, "The answer should not be empty")).min(2, "There should be at least 2 answers"),
   correctIndex: z.number().int().nonnegative("Correct index cannot be negative"),
   explanation: z.string().trim().min(10, "The explanation should be at least 10 characters long"),
-  reference: ReferenceSchema,
-}).refine((data: QuizQuestion) => data.correctIndex < data.answers.length, {
+  reference: referenceSchema,
+}).refine((data) => data.correctIndex < data.answers.length, {
   message: "The correct index should correspond to an answer",
   path: ["correctIndex"],
 });
 
-const QuizQuestionArraySchema = z.array(QuizQuestionSchema);
+export type QuizQuestion = z.infer<typeof quizQuestionSchema>;
+
+const quizQuestionArraySchema = z.array(quizQuestionSchema);
 
 
 export function getQuestions(): Promise<QuizQuestion[]> {
@@ -50,12 +38,15 @@ export function getQuestions(): Promise<QuizQuestion[]> {
   return fetch(request)
     .then((res) => res.json())
     .then((res) => {
-      const questionsArrayValidation = QuizQuestionArraySchema.safeParse(res);
-      if (!questionsArrayValidation.success) {
-        console.error("Invalid quiz data:", questionsArrayValidation.error.format());
+      const quizValidation = quizQuestionArraySchema.safeParse(res);
+      if (!quizValidation.success) {
+        console.error(
+          "Invalid quiz data:",
+          quizValidation.error.format(),
+        );
         throw new Error("The fetched quiz data was invalid");
       }
 
-      return questionsArrayValidation.data as QuizQuestion[];
-    });
+      return quizValidation.data;
+});
 }
