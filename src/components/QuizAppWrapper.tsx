11702a07cf5178420b5eb859fe9e2fd9ac34ddff
Andrei Comaneci <aco.aco.ro@gmail.com>
2025-03-01 19:02:40 +0100

    Put query client outside state
    * to ensure it's not created on re-rendering

--- src/components/QuizAppWrapper.tsx
+++ src/components/QuizAppWrapper.tsx
@@ -1,10 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import QuizApp from "./QuizApp";
-import { useState } from "react";
+
+const queryClient = new QueryClient();
 
 const QuizAppWrapper: React.FC = () => {
-    const [queryClient] = useState(() => new QueryClient());
-
     return (
         <QueryClientProvider client={queryClient}>
             <QuizApp />
