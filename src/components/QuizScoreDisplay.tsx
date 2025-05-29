f6c6f52d83f4d7686af396dc5d1edd55c72a1fa0
Andrei Comaneci <aco.aco.ro@gmail.com>
2025-05-29 19:01:59 +0200

    Make the closing statement depending on maximumScore
    * in case someone needs to change the number of questions

--- src/components/QuizScoreDisplay.tsx
+++ src/components/QuizScoreDisplay.tsx
@@ -5,17 +5,17 @@ type QuizScore = {
   maximumScore: number;
 };
 
-const closingStatement = (score: number): string => {
-  if (score <= 2)
+const closingStatement = (score: number, maximumScore: number): string => {
+  if (score <= 0.2 * maximumScore)
     return "Don’t worry, Rookie Lobstar! Every Lobstar starts somewhere, and you’re just getting your feet wet. Keep learning, practicing, and throwing! You’ll be lighting up the field in no time.";
 
-  if (score <= 4)
+  if (score <= 0.4 * maximumScore)
     return "Getting there, Aspiring Lobstar! You’ve made some good catches here. Dive back into the playbook and sharpen your game—you’re on the way to greatness!";
 
-  if (score <= 6)
+  if (score <= 0.6 * maximumScore)
     return "Nice one, Steady Lobstar! You’re showing solid skills and knowledge out there. A few more reps and you’ll be skying for the disc like a pro.";
 
-  if (score <= 8)
+  if (score <= 0.8 * maximumScore)
     return "Impressive, Rising Lobstar! You’ve clearly got a good grip on the game. Keep hustling and aiming for the stars—your ultimate future looks bright.";
 
   return "Outstanding, All-Star Lobstar! You’ve nailed it like a perfect hammer throw. Your knowledge is championship material—just like our Lobstars spirit on the field.";
@@ -30,7 +30,7 @@ export const QuizScoreDisplay: React.FC<QuizScore> = ({
       <p className="text-xl font-semibold text-neon-pink-dark">
         Your score: {score} / {maximumScore}
       </p>
-      <p>{closingStatement(score)}</p>
+      <p>{closingStatement(score, maximumScore)}</p>
     </>
   );
 };
