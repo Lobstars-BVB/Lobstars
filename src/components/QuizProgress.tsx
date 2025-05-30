e9bb29a9c0dbab119b9d8127c2a1eda8cb68315a
Andrei Comaneci <aco.aco.ro@gmail.com>
2025-05-30 10:15:46 +0200

    Replace space by padding

--- src/components/QuizProgress.tsx
+++ src/components/QuizProgress.tsx
@@ -11,8 +11,8 @@ export const QuizProgress: React.FC<ProgressProps> = ({
 }: ProgressProps) => {
   return (
     <div className="absolute right-3 top-1" id="progress-bar">
-      <label className="text-sm" htmlFor="quiz-progress">
-        Quiz progress:{" "}
+      <label className="text-sm pr-1" htmlFor="quiz-progress">
+        Quiz progress:
       </label>
       <progress id="quiz-progress" max={maxValue} value={currentValue}>
         { `${currentValue}/${maxValue}` }
