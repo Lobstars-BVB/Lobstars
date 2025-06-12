dead6c91b20f81ffbea9e4f3deabf54aca9a5d67
Andrei Comăneci <48902437+andreicomaneci@users.noreply.github.com>
2025-06-12 21:15:12 +0200

    Quiz updates based on feedback (#43)

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
+const imageSchema = z.object({
+  url: z.string().url("Invalid URL format"),
+  alt: z.string().min(1, "An alternative description is required"), // in case the URL fails to load
+});
+
 const referenceSchema = z.object({
   sourceName: z.string().trim().min(1, "The source should not be empty"),
   link: z
@@ -28,6 +33,7 @@ const quizQuestionSchema = z
       .trim()
       .min(10, "The explanation should be at least 10 characters long"),
     reference: referenceSchema,
+    image: imageSchema.nullish(),
   })
   .refine((data) => data.correctIndex < data.answers.length, {
     message: "The correct index should correspond to an answer",
