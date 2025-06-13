3cf366c7f113306732cea059a4baa09994444761
Andrei Comaneci <aco.aco.ro@gmail.com>
2025-06-13 17:30:50 +0200

    Apply formatter

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,16 +1,16 @@
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { CaretDownIcon } from "@radix-ui/react-icons";
 import "../styles/NavMenu.css";
 import * as React from "react";
 
 type LinkItem = {
-  type: 'link';
+  type: "link";
   href: string;
   label: string;
 };
 
 type DropdownItem = {
-  type: 'dropdown';
+  type: "dropdown";
   label: string;
   children: LinkItem[];
 };
@@ -28,36 +28,40 @@ const NavMenu: React.FC<NavMenuProps> = ({ menuItems }) => {
           if (menuItem.type === "link") {
             return (
               <NavigationMenu.Item key={menuItem.label}>
-                <NavigationMenu.Link href={menuItem.href} className="NavigationMenuLink">
+                <NavigationMenu.Link
+                  href={menuItem.href}
+                  className="NavigationMenuLink"
+                >
                   {menuItem.label}
                 </NavigationMenu.Link>
               </NavigationMenu.Item>
-            ) } else {
-              return (
-                <NavigationMenu.Item key={menuItem.label}>
-                  <NavigationMenu.Trigger className="NavigationMenuTrigger">
-                    {menuItem.label} <CaretDownIcon className="CaretDown" aria-hidden />
-                  </NavigationMenu.Trigger>
-                  <NavigationMenu.Content className="NavigationMenuContent">
-                    <ul className="DropdownList">
-                      {
-                        menuItem.children.map((linkItem: LinkItem) => {
-                          return (
-                            <li key={linkItem.label}>
-                              <NavigationMenu.Link asChild>
-                                <a className="ListItemLink" href={linkItem.href}>{linkItem.label}</a>
-                              </NavigationMenu.Link>
-                            </li>
-                          )
-                        })
-                      }
-                    </ul>
-                  </NavigationMenu.Content>
-                </NavigationMenu.Item>
-              )
-            }
-          })
-        }
+            );
+          } else {
+            return (
+              <NavigationMenu.Item key={menuItem.label}>
+                <NavigationMenu.Trigger className="NavigationMenuTrigger">
+                  {menuItem.label}{" "}
+                  <CaretDownIcon className="CaretDown" aria-hidden />
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content className="NavigationMenuContent">
+                  <ul className="DropdownList">
+                    {menuItem.children.map((linkItem: LinkItem) => {
+                      return (
+                        <li key={linkItem.label}>
+                          <NavigationMenu.Link asChild>
+                            <a className="ListItemLink" href={linkItem.href}>
+                              {linkItem.label}
+                            </a>
+                          </NavigationMenu.Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+            );
+          }
+        })}
 
         <NavigationMenu.Indicator className="NavigationMenuIndicator">
           <div className="Arrow" />
